50fa1fe408e6e6523754224dfce314ed10315740
Takaaki <dacchin.h081229@gmail.com>
2021-01-21 00:56:14 +0900

    implement update(show) page

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,21 @@
 import _ from 'lodash'
-import { READ_EVENTS, DELETE_EVENT } from '../actions'
+import {  READ_EVENTS,
+          DELETE_EVENT,
+          READ_EVENT,
+          UPDATE_EVENT,
+          CREATE_EVENT 
+        } from '../actions'
 
 // const initialState = { value: 0 }
 
 export default (events = {}, action) => {
   switch(action.type) {
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      console.log({events})
+      const data = action.response.data
+      return { ...events, [data.id]: data }
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
     case DELETE_EVENT:
